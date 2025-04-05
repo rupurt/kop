4d856ba06950d1afd01ff72e223e0a535e6a4dcd
Alex Kwiatkowski <alex+git@fremantle.io>
2025-04-05 06:34:53 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# kop
+![pypi](https://img.shields.io/pypi/v/kop.svg)
+![versions](https://img.shields.io/pypi/pyversions/kop.svg)
+
+Modern python kubernetes operators
+
+## Usage
+
+```console
+> pip install kop
+```
+
+## Getting Started
+
+- [Development](./docs/DEVELOPMENT.md)
+  - [setup](./docs/DEVELOPMENT.md#setup)
+  - [check](./docs/DEVELOPMENT.md#check)
+  - [dream2nix](./docs/DEVELOPMENT.md#dream2nix)
+  - [pypi](./docs/DEVELOPMENT.md#pypi)
+
+## License
+
+`kop` is released under the [MIT license](./LICENSE)
---
+++ default.nix
@@ -0,0 +1,45 @@
+{
+  config,
+  lib,
+  dream2nix,
+  ...
+}: {
+  imports = [
+    dream2nix.modules.dream2nix.WIP-python-pdm
+  ];
+
+  pdm = {
+    pyproject = ./pyproject.toml;
+  };
+
+  deps = {
+    nixpkgs,
+    self,
+    ...
+  }: {
+    inherit
+      (nixpkgs)
+      zlib
+      ;
+    python = nixpkgs.python312;
+    pythonPackages = nixpkgs.python312Packages;
+  };
+
+  mkDerivation = {
+    src = lib.cleanSourceWith {
+      src = lib.cleanSource ./.;
+      filter = name: type:
+        !(builtins.any (x: x) [
+          (lib.hasSuffix ".nix" name)
+          (lib.hasPrefix "." (builtins.baseNameOf name))
+          (lib.hasSuffix "flake.lock" name)
+        ]);
+    };
+    buildInputs = [
+      config.deps.python
+    ];
+  };
+
+  overrides = {
+  };
+}
